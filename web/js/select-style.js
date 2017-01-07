fdf712fccd074f8bfc5ca7beda2f18c5c3c3ca77
Smile <sisitymiteva@yahoo.com>
2017-01-07 17:19:14 +0200

    TG-27 #ready

--- web/js/select-style.js
+++ web/js/select-style.js
@@ -2,3 +2,7 @@ $('.selectpicker').selectpicker({
   style: 'btn-default',
   size: 5
 });
+
+$(document).on('ready', function(){
+    $('#input-5').rating({clearCaption: 'Бъди първия оценил този потребител!'});
+});
